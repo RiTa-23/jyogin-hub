9ef76efa6db251dbf098e82525c684c337ce1a81
RiTa-23 <rita.tifrt@gmail.com>
2026-05-09 16:40:05 +0900

    feat: メタデータにアイコンを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "JyoginHub",
   description: "NFC 出席確認 管理サーバー",
+  icons: "/Jyogin.png",
 };
 
 export default function RootLayout({
